80305b9631b89703ea203789c787b90f2d0e09e7
Sujal Garg <sujalgarg94@gmail.com>
2022-01-01 18:35:02 +0530

    1st commit

---
+++ 1st Jan 2022/ sum of elements in list.py
@@ -0,0 +1,4 @@
+# Q sum of elements in list
+a=[2,4,5,6]
+print(a[0]+a[1]+a[2]+a[3])  # --> adding elements manually
+print(sum(a))      # --> sum() is a mehtod of list
---
+++ 1st Jan 2022/area of circle.py
@@ -0,0 +1,7 @@
+
+pi = 3.14
+radius  = input("enter the radius: ")
+radius = float(radius)
+area = (pi*radius*radius)
+
+print(area)
