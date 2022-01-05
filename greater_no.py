93cce890efe02214bdb652f64919a5eefc2d84fd
Sujal Garg <sujalgarg94@gmail.com>
2022-01-05 20:00:22 +0530

    .

---
+++ greater_no.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the number 1: "))
+b=int(input("Enter the number 2: "))
+c=int(input("Enter the number 3: "))
+d=int(input("Enter the number 4: "))
+if(a>b):
+    X=a
+else:
+    X=b
+if(c>d):
+    Y=c
+else:
+    Y=d
+if(X>Y):
+    print(X ," is greater")
+else:
+    print(Y ," is greater")                    
