bc319f0e080535f31c1ef81d7db7dc51c7985bd7
Sujal Garg <sujalgarg94@gmail.com>
2022-01-06 20:07:14 +0530

    .

--- 6th Jan 2022/continue_for.py
+++ 6th Jan 2022/continue_for.py
@@ -1,4 +1,5 @@
 for n in range(5,15):
-    print(n)
-    if n==10:
-        continue
+   
+ if n==10:
+    continue
+ print(n)  #here continue statement skips the print statement when i=10 as this skip the next step and start from the loop again 
