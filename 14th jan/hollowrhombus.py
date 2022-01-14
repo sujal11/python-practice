aabb5adc7cd08f7eda5779caa366ca9efa615ec6
Sujal Garg <sujalgarg94@gmail.com>
2022-01-14 19:06:51 +0530

    .

---
+++ 14th jan/hollowrhombus.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number "))
+m=int(input("Enter the number"))
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if(i==n or j==m or j==1 or i==1):
+            print("*",end="")
+        else:
+            print(" ",end="") 
+    print()    
+
+    
+
