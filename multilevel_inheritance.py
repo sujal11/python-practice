1eb7134f5c5d63ccda5e048a85965cd79851d65b
Sujal Garg <sujalgarg94@gmail.com>
2022-01-18 19:37:12 +0530

    .

--- multilevel_inheritance.py
+++ multilevel_inheritance.py
@@ -1,15 +1,15 @@
-class Employee:
+class Employee:     # --> Parents class
     company="Google"
     def __init__(self,name):
         self.name=name       
         
-    #  Mulitilevel Inheritance --> when there is more than one derived class of single base class       
+    #  Mulitilevel Inheritance --> when there is derived class is also a parents for another derived class      
 
-class Programmer(Employee):  
+class Programmer(Employee):   # --> Derived class of class Employee and parent class of class Student
     def getDetails(self):   
         print(f"The Employee Name Is {self.name} And His Company Is {self.company}")
         
-class Student(Employee):  
+class Student(Programmer):   # --> Derived class of class Programmer
     def getInfo(self):
         self.company="Youtube"
         print(f"The Student Name Is {self.name} And His Company Is {self.company}") 
